ff3af7080296353d64a4524db6304d3a1a87773f
Alexander_Tikhonov <tihas75@mail.ru>
2024-08-05 15:50:36 +0300

    fix_2 newapp.urls.py

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from newapp.apps import NewappConfig
-from newapp.views import home
+from newapp.views import home, contacts
 
 app_name = NewappConfig.name
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('', contacts, name='contacts')
 ]
